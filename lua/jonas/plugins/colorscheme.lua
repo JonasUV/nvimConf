d0fed3a1d375dce31c771bc5d283997de6960bfe
Jonas(GalaxyBook) <trashmaster54@gmail.com>
2023-11-24 17:31:10 +0100

    changed to transparend colortheme

--- lua/jonas/plugins/colorscheme.lua
+++ lua/jonas/plugins/colorscheme.lua
@@ -1,10 +1,30 @@
 return {
-  {
-    "bluz71/vim-nightfly-guicolors",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme nightfly]])
-    end,
- },
- }
+  --{
+    --  "bluz71/vim-nightfly-guicolors",
+    --priority = 1000, -- make sure to load this before all the other start plugins
+    --config = function()
+       -- load the colorscheme here
+      --vim.cmd([[colorscheme nightfly]])
+      --end,
+    --},
+
+    {
+      'uloco/bluloco.nvim',
+      lazy = false,
+      priority = 1000,
+      dependencies = { 'rktjmp/lush.nvim' },
+      config = function()
+        require("bluloco").setup({
+          style = "dark",               -- "auto" | "dark" | "light"
+          transparent = true,
+          italics = false,
+          terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
+          guicursor   = true,
+        })
+
+      vim.opt.termguicolors = true
+      vim.cmd('colorscheme bluloco')
+      -- your optional config goes here, see below.
+      end,
+    },
+}
