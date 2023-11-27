f56c6d29c0cc52656ebe9f99da59e7ad05aebade
Jonas(GalaxyBook) <trashmaster54@gmail.com>
2023-11-27 11:01:53 +0100

    added telescope and changed color theme

--- lua/jonas/plugins/colorscheme.lua
+++ lua/jonas/plugins/colorscheme.lua
@@ -15,7 +15,7 @@ return {
       dependencies = { 'rktjmp/lush.nvim' },
       config = function()
         require("bluloco").setup({
-          style = "dark",               -- "auto" | "dark" | "light"
+        style = "auto",               -- "auto" | "dark" | "light"
           transparent = true,
           italics = false,
           terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
