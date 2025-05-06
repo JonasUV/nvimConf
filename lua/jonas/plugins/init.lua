bf6ca59b894997abd00a5d1fa8023f7726f1ba30
JonasUV <jonas.wittler@gmail.com>
2025-05-06 17:52:03 +0200

    add vimsuda

--- lua/jonas/plugins/init.lua
+++ lua/jonas/plugins/init.lua
@@ -1,4 +1,6 @@
 -- for plugins that dont need any extra configuration
 return {
-	"christoomey/vim-tmux-navigator" -- tmux & split window navigator
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigator
+  "elkowar/yuck.vim",
+  "lambdalisue/vim-suda"
 }
