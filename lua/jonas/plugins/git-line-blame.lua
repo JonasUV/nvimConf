e11205395e098f6f10a4ff2caa6fabc26cf67f6d
JonasUV <jonas.wittler@gmail.com>
2025-06-17 20:27:21 +0200

    enhance line blame

--- lua/jonas/plugins/git-line-blame.lua
+++ lua/jonas/plugins/git-line-blame.lua
@@ -10,8 +10,8 @@ return {
         -- your configuration comes here
         -- for example
         enabled = true,  -- if you want to enable the plugin
-        message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-        date_format = "%d-%m-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+        message_template = "\t~~ <author> • <summary> • <date> • <<sha>>", -- template for the blame message, check the Message template section for more options
+        date_format = "%d.%m.%y %H:%M:%S", -- template for the date, check Date format section for more options
         virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
     },
  
